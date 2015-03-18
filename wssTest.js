fe22daee4209fd71192795049e28f5d54ce98b32
shhider <734459639@qq.com>
2015-03-18 19:23:01 +0800

    found a similar game

--- wssTest.js
+++ wssTest.js
@@ -1,10 +1,14 @@
 var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 8000});
+    wss = new WebSocketServer({port: 8080});
 
 wss.on('connection', function(ws){
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', function(data) {
     ws.send(data);
+  });
+
+  ws.on('message', function(msg){
+    console.log(msg);
   });
 });
